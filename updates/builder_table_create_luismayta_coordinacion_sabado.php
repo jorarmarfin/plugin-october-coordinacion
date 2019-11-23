eaacf57a3a1aee8a0df776b03118e9aeecf5d140
Luis Mayta <luis.mayta@gmail.com>
2019-11-23 01:31:54 -0500

    Sabado init

---
+++ updates/builder_table_create_luismayta_coordinacion_sabado.php
@@ -0,0 +1,29 @@
+<?php namespace LuisMayta\Coordinacion\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class BuilderTableCreateLuismaytaCoordinacionSabado extends Migration
+{
+    public function up()
+    {
+        Schema::create('luismayta_coordinacion_sabado', function($table)
+        {
+            $table->engine = 'InnoDB';
+            $table->increments('id');
+            $table->string('oracion_inical');
+            $table->string('musica');
+            $table->string('oracion_ponente');
+            $table->string('tema');
+            $table->string('oracion_ofrendas');
+            $table->string('titulo');
+            $table->date('fecha');
+            $table->text('observacion');
+        });
+    }
+    
+    public function down()
+    {
+        Schema::dropIfExists('luismayta_coordinacion_sabado');
+    }
+}
