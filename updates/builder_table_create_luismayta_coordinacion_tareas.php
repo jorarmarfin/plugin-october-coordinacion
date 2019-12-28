da4156e13656921c15c82d7cc5ee565367231431
Luis Mayta <luis.mayta@gmail.com>
2019-12-28 01:18:22 -0500

    Tareas

---
+++ updates/builder_table_create_luismayta_coordinacion_tareas.php
@@ -0,0 +1,24 @@
+<?php namespace LuisMayta\Coordinacion\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class BuilderTableCreateLuismaytaCoordinacionTareas extends Migration
+{
+    public function up()
+    {
+        Schema::create('luismayta_coordinacion_tareas', function($table)
+        {
+            $table->engine = 'InnoDB';
+            $table->increments('id');
+            $table->text('descripcion');
+            $table->date('fecha');
+            $table->integer('idhermano');
+        });
+    }
+    
+    public function down()
+    {
+        Schema::dropIfExists('luismayta_coordinacion_tareas');
+    }
+}
