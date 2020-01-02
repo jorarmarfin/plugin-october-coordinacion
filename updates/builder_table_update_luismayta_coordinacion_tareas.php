6e0aff60f9b66324a911100d8a5f8f31017bc313
Luis Mayta <luis.mayta@gmail.com>
2020-01-01 23:41:41 -0500

    Tareas

---
+++ updates/builder_table_update_luismayta_coordinacion_tareas.php
@@ -0,0 +1,23 @@
+<?php namespace LuisMayta\Coordinacion\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class BuilderTableUpdateLuismaytaCoordinacionTareas extends Migration
+{
+    public function up()
+    {
+        Schema::table('luismayta_coordinacion_tareas', function($table)
+        {
+            $table->string('accion');
+        });
+    }
+    
+    public function down()
+    {
+        Schema::table('luismayta_coordinacion_tareas', function($table)
+        {
+            $table->dropColumn('accion');
+        });
+    }
+}
